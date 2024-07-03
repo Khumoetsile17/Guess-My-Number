f04794bffa08f8930b3865b7c7ad1bb722b38196
Khumoetsile17 <maropekhumoetsile@gmail.com>
2024-07-03 11:36:19 +0200

    Added rest functionality, so that player can start new game

--- script.js
+++ script.js
@@ -50,16 +50,13 @@ console.log(document.querySelector(".guess").value);
 */
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-let score = 25;
+let score = 20;
 document.querySelector(".score").textContent = score;
 
 document.querySelector(".btn").addEventListener("click", function () {
-  /*whenever we get something
-  from the user interface, for example, from an input field,it usually always is a string hence convert and save to some variable guess.*/
+  /*input from the user interface,is a string.*/
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
-  /*whenever we get something
-  from the user interface, for example, from an input field,it usually always is a string.*/
 
   if (!guess) {
     document.querySelector(".message").textContent = " ⛔ No number entered!";
@@ -70,22 +67,32 @@ document.querySelector(".btn").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
 
     document.querySelector(".Secret-number").style.width = "30rem";
-  } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "📈 Too high!";
-    score--;
-    document.querySelector(".score").textContent = score;
-  } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "📉 Too low!";
-    score--;
-    document.querySelector(".score").textContent = score;
+  } else {
+    if (score > 0) {
+      if (guess > secretNumber) {
+        document.querySelector(".message").textContent = "📈 Too high!";
+      } else if (guess < secretNumber) {
+        document.querySelector(".message").textContent = "📉 Too low!";
+      }
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+    }
   }
 });
 
-/*
-We selected button using querySelector,
-Then we use the addEventListner method on that element to attach an event handler(function)
-This function will only be called
-as soon as the event happens.
+//this func is handler function
+document.querySelector(".btn-again").addEventListener("click", function () {
+  document.querySelector(".Secret-number").textContent = "?";
 
+  document.querySelector(".score").textContent = 20;
 
- */
+  document.querySelector(".message").textContent = "Start guessing...";
+
+  document.querySelector(".guess").value = 0;
+
+  document.querySelector("body").style.backgroundColor = "#222";
+
+  document.querySelector(".Secret-number").style.width = "15rem";
+});
